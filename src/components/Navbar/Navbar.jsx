56b621caa0688003dda199832278dbf743e72053
raushan <kraushan022@gmail.com>
2025-03-12 10:03:47 +0530

    add smooth scroll

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.svg";
 import underline from "../../assets/nav_underline.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
@@ -10,23 +11,34 @@ const Navbar = () => {
       <img src={logo} alt="" />
       <ul className="nav-menu">
         <li>
-          <p onClick={() => setMenu("home")}>Home</p>
+          <AnchorLink className="anchor-link" href="#home">
+            <p onClick={() => setMenu("home")}>Home</p>
+          </AnchorLink>
+
           {menu === "home" ? <img src={underline} /> : <></>}
         </li>
         <li>
-          <p onClick={() => setMenu("about")}>About Me</p>
+          <AnchorLink className="anchor-link" offset={50} href="#about">
+            <p onClick={() => setMenu("about")}>About Me</p>
+          </AnchorLink>
           {menu === "about" ? <img src={underline} /> : <></>}
         </li>
         <li>
-          <p onClick={() => setMenu("services")}>Servies</p>
+          <AnchorLink className="anchor-link" offset={50} href="#services">
+            <p onClick={() => setMenu("services")}>Servies</p>
+          </AnchorLink>
           {menu === "services" ? <img src={underline} /> : <></>}
         </li>
         <li>
-          <p onClick={() => setMenu("work")}>Portfolio</p>
+          <AnchorLink className="anchor-link" offset={50} href="#work">
+            <p onClick={() => setMenu("work")}>Portfolio</p>
+          </AnchorLink>
           {menu === "work" ? <img src={underline} /> : <></>}
         </li>
         <li>
-          <p onClick={() => setMenu("contact")}>Contact</p>
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            <p onClick={() => setMenu("contact")}>Contact</p>
+          </AnchorLink>
           {menu === "contact" ? <img src={underline} /> : <></>}
         </li>
       </ul>
